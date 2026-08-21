b2f43b50670a7ad4592431ba49674c4efc6b29a5
samrowland472-lang <samrowland472@gmail.com>
2026-08-21 23:26:28 +0100

    Session View: live clip grid with quantized launch

--- netlify/edge-functions/flick-inject.js
+++ netlify/edge-functions/flick-inject.js
@@ -12,15 +12,21 @@ export default async (request, context) => {
   const type = res.headers.get("content-type") || "";
   if (!type.includes("text/html")) return res;
 
-  const html = await res.text();
-  const injected = html.includes("/js/flick.js")
-    ? html
-    : html.replace(
-        "</body>",
-        '<script src="/js/flick.js" data-name="Flick" data-site="The Voice" defer></script>\n</body>',
-      );
+  let html = await res.text();
+  if (!html.includes("/js/flick.js")) {
+    html = html.replace(
+      "</body>",
+      '<script src="/js/flick.js" data-name="Flick" data-site="The Voice" defer></script>\n</body>',
+    );
+  }
+  if (!html.includes("/js/daw-session.js")) {
+    html = html.replace(
+      "</body>",
+      '<script src="/js/daw-session.js" defer></script>\n</body>',
+    );
+  }
 
-  return new Response(injected, {
+  return new Response(html, {
     status: res.status,
     headers: res.headers,
   });
