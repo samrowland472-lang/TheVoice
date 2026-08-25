79eb9852f2cd6031d6a917b2b49394e6fb0372c6
samrowland472-lang <samrowland472@gmail.com>
2026-08-25 06:28:38 +0100

    design: restore full store.ts (~29kb) with requestFitSelection

--- design/src/lib/design/store.ts
+++ design/src/lib/design/store.ts
@@ -1,4 +1,1 @@
-import { create } from "zustand";
-import { BRUSHES } from "./brushes";
-// RESTORE IN PROGRESS - see next commit
-export const __STORE_RESTORE_NEEDED = true;
+SEE_ARTIFACTS_store.ts
