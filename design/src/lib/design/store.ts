9af6ab9d679e7af957c72837f4098cf1953769cb
samrowland472-lang <samrowland472@gmail.com>
2026-09-02 04:45:34 +0100

    design: restore Zustand store via store-impl

--- design/src/lib/design/store.ts
+++ design/src/lib/design/store.ts
@@ -1,21 +1,7 @@
-import { create } from "zustand";
-import { BRUSHES } from "./brushes";
-import { formatById } from "./formats";
-import { aabb } from "./geometry";
-import { uid } from "./id";
-import { cloneNode, paintLayer, shape, text } from "./node-factory";
-import { deleteDoc, loadBrand, loadDoc, loadIndex, patchIndex, saveBrand, saveDoc } from "./persist";
-import { exportPng } from "./export";
-import { paletteName } from "./palette";
-import { blankDocument, instantiateTemplate } from "./templates";
-import type { BooleanOp } from "./boolean-ops";
-import type { PathEditHit } from "./path-edit";
-import type {
-  BrandKit,
-  BrushSettings,
-  DesignDocument,
-  DesignNode,
-  ProjectMeta,
-  Tool,
-  Viewport,
-} from "./types";
+export {
+  useDesign,
+  makeShape,
+  makeText,
+  ensurePaintLayer,
+} from "./store-impl";
+export type { ViewIntent } from "./store-impl";
